ff105aebae68e450a88178089973547774ce7815
Urich Oliveira <oliveira.urich@gmail.com>
2020-09-24 23:18:51 -0300

    Fix core scheduler

--- main/ext/scheduler/core.py
+++ main/ext/scheduler/core.py
@@ -14,19 +14,10 @@ class FlaskScheduler(BackgroundScheduler):
 	def __init__(self, app=None, **options):
 		super().__init__(**options)
 		self._hostname = socket.gethostname().lower()
-		self._app = app
 
 	@property
 	def hostname(self):
 		return self._hostname
-
-	@property
-	def app(self):
-		return self._app
-
-	@app.setter
-	def app(self, value):
-		self.app = value
 
 	def load_config(self):
 		options = dict()
@@ -44,13 +35,12 @@ class FlaskScheduler(BackgroundScheduler):
 		}
 		timezone = settings.TIMEZONE
 
-		setattr(options, 'jobstores', jobstores)
-		setattr(options, 'executors', executors)
-		setattr(options, 'job_defaults', job_defaults)
-		setattr(options, 'timezone', timezone)
+		options['jobstores'] = jobstores
+		options['executors'] = executors
+		options['job_defaults'] = job_defaults
+		options['timezone'] = timezone
 
 		self.configure(**options)
 
 	def init_app(self, app):
-		self.app = app
 		self.load_config()
